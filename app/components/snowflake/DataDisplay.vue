a65b5e761968415e8c345dc8dbc583756d3d437c
ItsRauf <31735267+ItsRauf@users.noreply.github.com>
2025-06-03 11:18:28 -0500

    feat: add snowflake decoder

---
+++ app/components/snowflake/DataDisplay.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  valid: Boolean,
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <span>
+    <h2 class="text-2xl text-highlighted font-bold">
+      {{ props.title }}
+    </h2>
+    <h1 v-if="props.valid" class="text-4xl text-primary font-bold">
+      {{ props.value }}
+    </h1>
+    <USkeleton v-else class="text-4xl h-lh mt-1" />
+  </span>
+</template>
